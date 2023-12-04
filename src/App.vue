03d995f3b35bc4c50cfc04ab56cd2360b1830158
Gabriel Rocha <gabriel@t2tsolucoes.com>
2023-12-04 00:06:01 -0300

    feat: add new files to project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <h1>{{ fullName }}</h1>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      name: 'Gabriel',
+      lastName: 'Rocha'
+    }
   },
-});
+  computed: {
+    fullName(): string {
+      return `${this.name} ${this.lastName}`
+    }
+  }
+})
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
